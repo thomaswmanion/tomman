653af60381260a4af6d721d3492aa4af31920dd9
Tom Manion <thomaswmanion@gmail.com>
2019-08-11 02:27:38 -0500

    Server routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,5 +2,6 @@ import { ServerRoute } from "hapi";
 import { GenerateHandler } from "./generate/generate.handler";
 
 export const routes: ServerRoute[] = [
-    { path: 'generate', method: 'post', handler: new GenerateHandler().handle }
+    { path: '/generate', method: 'post', handler: new GenerateHandler().handle },
+    { path: '/test', method: 'get', handler: new GenerateHandler().handle }
 ];
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import { Server } from 'hapi';
 import { routes } from './routes';
 
-const server = new Server({ load: { sampleInterval: 1000 } });
+const server = new Server({ load: { sampleInterval: 1000 }, port: 4300 });
 server.route(routes);
 server.start().catch(e => console.error(e));
+console.log('Server Started');
